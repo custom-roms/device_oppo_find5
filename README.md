d1d3ab3ac0de7104faa42799b7e0054172ccc1ef
wolverine2k <nareshtechs@gmail.com>
2017-01-19 09:57:06 +0100

    Modified for AOKP 7.0

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Device tree for Oppo Find5
 
 Copyright 2013, The CyanogenMod Project
 
+Adpated to work with AOKP 7
--- aokp.mk
+++ aokp.mk
@@ -2,11 +2,11 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 $(call inherit-product, device/oppo/find5/full_find5.mk)
 
-PRODUCT_NAME := cm_find5
+PRODUCT_NAME := aokp_find5
 PRODUCT_DEVICE := find5
 
 #PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT=OPPO/oppo_12069/FIND5:4.1.1/JRO03L/1362469752:user/release-keys PRIVATE_BUILD_DESC="msm8960-user 4.1.1 JRO03L eng.oppo.20130328.172033 release-keys"
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT=OPPO/cm_find5/find5:6.0/MDB08M/9648e16a59:eng/test-keys PRIVATE_BUILD_DESC="Wolverine2k-cm13.0/6.0 MDB08M eng.oppo.20151208 keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT=OPPO/aokp_find5/find5:7.0/MDB08M/9648e16a59:eng/test-keys PRIVATE_BUILD_DESC="Wolverine2k-aokp/7.0 MDB08M eng.oppo.20170119 keys"
 
 #PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT=OPPO/cm_find5/find5:5.1.1/LMY48G/9648e16a59:eng/test-keys PRIVATE_BUILD_DESC="Wolverine2k-cm12.1/5.1.1 LMY48G eng.oppo.20150729 keys"
 #PRODUCT_BUILD_PROP_OVERRIDES += USER=android-build
